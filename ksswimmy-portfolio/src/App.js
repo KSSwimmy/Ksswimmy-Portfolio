78204ae12f96b7c0511cfde3a7914ad1133fae78
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2019-11-02 23:38:04 -0400

    :fire: TEST PARALLAX IS WORKING!! WOOOOOHHHH!!
    TEST PARALLAX IS WORKING!! WOOOOOHHHH!!

--- ksswimmy-portfolio/src/App.js
+++ ksswimmy-portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TitlePanel from './components/Panels/TitlePanel'
 import TopNamePanel from './components/Panels/TopNamePanel'
 import './index.css'
+import ParallaxEffect from './components/Planets/ParallaxEffect';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <TitlePanel /> 
       <TopNamePanel />
+      <ParallaxEffect />
     </div>
   );
 }
