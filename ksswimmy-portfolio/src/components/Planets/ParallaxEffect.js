d8c31f72346db13e7a4fa41c8eb23e0846b3e2bb
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2019-11-24 22:17:07 -0500

    :boom: adding props to planets for parallax
    adding props to planets for parallax

--- ksswimmy-portfolio/src/components/Planets/ParallaxEffect.js
+++ ksswimmy-portfolio/src/components/Planets/ParallaxEffect.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 import styled from 'styled-components'
+import Grid from '@material-ui/core/Grid';
 import TheSun from '../../SVG/sun.svg'
 import Earth from '../../SVG/earth.svg'
 import Jupiter from '../../SVG/jupiter.svg'
+import Mars from '../../SVG/mars.svg'
 
-const Tester = styled.h1`
-    color: white
+const Tester = styled.div`
+    
+
 `
 
-const TheSunn = styled.div`
-display: flex;
-justify-content: flex-start;
+const PlanetDiv = styled.div`
+.ChildContainer
+
 `
 
 
@@ -19,31 +22,59 @@ justify-content: flex-start;
 const ParallaxEffect = () => {
     let parallax;
   return (
-<Tester>
-    <Parallax className="layer" pages={3} ref={ref => parallax = ref}>
-      <ParallaxLayer offset={0} speed={0}>
-        <TheSunn><img
-        style={{width:'100%'}}  
-        src={TheSun} 
-        alt="sun" /></TheSunn>
-      </ParallaxLayer>
-      <ParallaxLayer offset={1.2} speed={1}>
-        <span><img
-        // style={{width:'100%'}}  
-        src={Earth} 
-        alt="sun" /></span>
-      </ParallaxLayer>
-      <ParallaxLayer offset={0.9} speed={3}>
-        <span><img
-        // style={{width:'100%'}}  
-        src={Jupiter} 
-        alt="sun" /></span>
-      </ParallaxLayer>
-      <ParallaxLayer speed={-0.2} offset={1.3}>
-        <span>I'm going in the other direction!</span>
-      </ParallaxLayer>
-    </Parallax>
-</Tester>
+
+
+    <Parallax className="layer" pages={4} ref={ref => parallax = ref}>
+
+    <Tester>
+
+      <PlanetDiv className="sunDiv">
+        <ParallaxLayer offset={0} speed={0}>
+          <span>
+            <img
+          style={{width:'100%'}, {paddingBottom: 700}}  
+          src={TheSun} 
+          alt="sun" />
+          </span>
+        </ParallaxLayer>
+      </PlanetDiv>
+
+      <PlanetDiv className="earthDiv">
+        <ParallaxLayer offset={1.2} speed={1}>
+          <span><img
+          style={{width:'100%'}}  
+          src={Earth} 
+          alt="Earth" />
+          </span>
+        </ParallaxLayer>
+      </PlanetDiv>
+
+      <PlanetDiv className="jupiterDiv">
+        <ParallaxLayer offset={0.9} speed={3}>
+          <span>
+            <img
+          style={{width:'100%'}}  
+          src={Jupiter} 
+          alt="Jupiter" /></span>
+        </ParallaxLayer>
+      </PlanetDiv>
+
+      <PlanetDiv className="jupiterDiv">
+        <ParallaxLayer speed={-0.2} offset={1.3}>
+          <span>
+              <img
+            style={{width:'100%'}}  
+            src={Mars} 
+            alt="Mars" />
+            </span>
+        </ParallaxLayer>
+      </PlanetDiv>
+
+    </Tester>
+
+  </Parallax>
+
+
   );
 }
 
